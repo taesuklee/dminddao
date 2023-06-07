f4ea5e6a9feed0f5c23f4b69e574456c8f0a2b94
taesuklee <taesuk.lee.data@tuta.io>
2023-06-07 16:33:12 +0200

    edit deploy file

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,21 +1,12 @@
 import { ethers } from 'hardhat'
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000)
-  const unlockTime = currentTimestampInSeconds + 60
+  const Marketplace = await ethers.getContractFactory('Marketplace')
+  const marketplace = await Marketplace.deploy()
 
-  const lockedAmount = ethers.utils.parseEther('0.001')
+  await marketplace.deployed()
 
-  const Lock = await ethers.getContractFactory('Lock')
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount })
-
-  await lock.deployed()
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  )
+  console.log(`Deployed contract address ${marketplace.address}`)
 }
 
 main().catch((error) => {
